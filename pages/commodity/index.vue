2827b1a3a5501abc925743959a711cc2e69c405f
zuozy999 <zuozy@ti-net.com.cn>
2022-03-16 15:59:37 +0800

    feat:#init 初始化

---
+++ pages/commodity/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="kmj">
+		首页
+		<view class="aa">
+			局域
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.kmj {
+	font-size: 30upx;
+	.aa {
+		font-size: 30upx;
+	}
+}
+</style>
